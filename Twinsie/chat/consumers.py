c4af3cf19f5dd07533075fd00b67e544703c97e8
Pham Anh Bao Duy <so4.lop7@gmail.com>
2023-09-04 19:36:12 +0800

    Implementation of the chat feature
    Adding in the newly tested chat features that uses Websocket and Redis server to store the messages

---
+++ Twinsie/chat/consumers.py
@@ -0,0 +1,27 @@
+from chat.models import Chat, Room
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth.models import User
+
+        
+
+class ChatRoomConsumer(AsyncWebsocketConsumer):
+
+    """Connect"""
+    async def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+        await self.accept()
+
+    """Disconnect"""
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+    
