4db953aeac7f58991349ee6ea1b1200c98595526
Pham Anh Bao Duy <so4.lop7@gmail.com>
2023-09-09 22:09:52 +0800

    Updating to utilize Daphne for ASGI

--- Twinsie/asgi.py
+++ Twinsie/asgi.py
@@ -7,8 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
+# import os
+# from django.core.asgi import get_asgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Twinsie.settings')
+# application = get_asgi_application()
 import os
-from django.core.asgi import get_asgi_application
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Twinsie.settings')
-application = get_asgi_application()
+django.setup()
+application = get_default_application()
