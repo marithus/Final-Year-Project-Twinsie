d3103a1b32d12d9c5c6f7ace275fea660d87847a
Pham Anh Bao Duy <so4.lop7@gmail.com>
2023-09-09 23:21:06 +0800

    added static files

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
+release: python manage.py makemigrations
 release: python manage.py migrate
 web: daphne Twinsie.asgi:application --port $PORT --bind 0.0.0.0 -v2
 worker: python manage.py runworker channels --settings=Twinsie.settings -v2
--- Twinsie/settings.py
+++ Twinsie/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -167,6 +168,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
