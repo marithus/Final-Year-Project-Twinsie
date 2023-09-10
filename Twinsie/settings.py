89750f4357898cae47da14cab4d1583ebf753c00
Pham Anh Bao Duy <so4.lop7@gmail.com>
2023-09-10 11:31:47 +0800

    Update settings.py

--- Twinsie/settings.py
+++ Twinsie/settings.py
@@ -175,7 +175,7 @@ STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 
